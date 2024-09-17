b908171e071a2dce3e47be1d1af09462f9919047
Jhinseng <kisesige16@gmail.com>
2024-09-17 16:43:54 -0700

    Fixed file system issues

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update() {
+        // Mouse Input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation -= mouseY;
+
+        // Prevents camera going beyond feet-to-apex of sky view
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Camera Rotation and Orientation
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        orientation.Rotate(Vector3.up * mouseX);
+        
+    }
+}
