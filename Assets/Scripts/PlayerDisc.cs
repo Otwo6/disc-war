6ab6fded75cfbe2721c383fb22377ffbb09587a6
Otwo06 <108424244+Otwo06@users.noreply.github.com>
2024-09-18 11:07:42 -0700

    Fixed Non-Deleting Disc + Added CallBack Delay

--- Assets/Scripts/PlayerDisc.cs
+++ Assets/Scripts/PlayerDisc.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class PlayerDisc : MonoBehaviour
 {
-    [SerializeField] GameObject discPrefab;
-    [SerializeField] GameObject throwLocation;
-    [SerializeField] GameObject camera;
-    [SerializeField] GameObject handDisc;
+	[SerializeField] GameObject discPrefab;
+	[SerializeField] GameObject throwLocation;
+	[SerializeField] GameObject camera;
+	[SerializeField] GameObject handDisc;
+	[SerializeField] float callBackWaitTime;
 
-    DiscScript disc;
-    bool hasDisc = true;
+	DiscScript disc;
+	bool hasDisc = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown("f"))
-        {
-            if(hasDisc)
-            {
-                handDisc.active = false;
-                disc = Instantiate(discPrefab, throwLocation.transform.position, transform.rotation).GetComponent<DiscScript>();
-                disc.SetForwardDirection(camera.transform.forward);
-                hasDisc = false;
-            }
-            else
-            {
-                Destroy(disc);
-                handDisc.active = true;
-                hasDisc = true;
-            }
-        }
-    }
+	void Update()
+	{
+		if(Input.GetKeyDown("f"))
+		{
+			if(hasDisc)
+			{
+				handDisc.active = false;
+				disc = Instantiate(discPrefab, throwLocation.transform.position, transform.rotation).GetComponent<DiscScript>();
+				disc.SetForwardDirection(camera.transform.forward);
+				hasDisc = false;
+			}
+			else
+			{
+				Destroy(disc.gameObject);
+				StartCoroutine(CallBackDisc());
+			}
+		}
+	}
+
+	private IEnumerator CallBackDisc()
+	{
+		yield return new WaitForSeconds(callBackWaitTime);
+		handDisc.active = true;
+		hasDisc = true;
+	}
 }
