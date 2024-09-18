0c71814c64558a452a543c6b8576de77d06eccaa
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-09-17 21:35:29 -0700

    Added Disc Representation in Hand

--- Assets/Scripts/PlayerDisc.cs
+++ Assets/Scripts/PlayerDisc.cs
@@ -7,14 +7,29 @@ public class PlayerDisc : MonoBehaviour
     [SerializeField] GameObject discPrefab;
     [SerializeField] GameObject throwLocation;
     [SerializeField] GameObject camera;
+    [SerializeField] GameObject handDisc;
+
+    DiscScript disc;
+    bool hasDisc = true;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("f"))
         {
-            DiscScript disc = Instantiate(discPrefab, throwLocation.transform.position, transform.rotation).GetComponent<DiscScript>();
-            disc.SetForwardDirection(camera.transform.forward);
+            if(hasDisc)
+            {
+                handDisc.active = false;
+                disc = Instantiate(discPrefab, throwLocation.transform.position, transform.rotation).GetComponent<DiscScript>();
+                disc.SetForwardDirection(camera.transform.forward);
+                hasDisc = false;
+            }
+            else
+            {
+                Destroy(disc);
+                handDisc.active = true;
+                hasDisc = true;
+            }
         }
     }
 }
