54162cc34cec15e77b5d6a362f1cfe4e46674cd3
Jhinseng <kisesige16@gmail.com>
2024-09-19 20:30:52 -0700

    Implemented Relay and Network for Multiplayer Host Connection

---
+++ Assets/Scripts/RelayManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+
+public class RelayManager : MonoBehaviour
+{
+
+    [SerializeField] Button hostButton;
+    [SerializeField] Button joinButton;
+    [SerializeField] TMP_InputField joinInput;
+    [SerializeField] TextMeshProUGUI codeText;
+
+    // Start is called before the first frame update
+    async void Start() {
+        await UnityServices.InitializeAsync();
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        hostButton.onClick.AddListener(CreateRelay);
+        joinButton.onClick.AddListener(() => JoinRelay(joinInput.text));
+    }
+
+    // Update is called once per frame
+    async void CreateRelay() {
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+        string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        codeText.text = "Code: " + joinCode; 
+
+        var relayServerData = new RelayServerData(allocation, "dtls");
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartHost();
+    }
+
+    async void JoinRelay(string joinCode) {
+        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        var relayServerData = new RelayServerData(joinAllocation, "dtls");
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartClient();
+    }
+}
