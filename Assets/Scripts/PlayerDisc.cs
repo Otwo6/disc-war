46cf0736c30b5d8c523cdd4e9b95cf205e3e6df9
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-09-21 08:14:08 -0700

    Added Client Side Request Server Despawning

--- Assets/Scripts/PlayerDisc.cs
+++ Assets/Scripts/PlayerDisc.cs
@@ -1,108 +1,101 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
 public class PlayerDisc : NetworkBehaviour
 {
-	[SerializeField] GameObject discPrefab;
-	[SerializeField] GameObject throwLocation;
-	[SerializeField] GameObject camera;
-	[SerializeField] GameObject handDisc;
-	[SerializeField] float callBackWaitTime;
+    [SerializeField] GameObject discPrefab;
+    [SerializeField] Transform throwLocation; // Use Transform for position/rotation
+    [SerializeField] GameObject camera;
+    [SerializeField] GameObject handDisc;
+    [SerializeField] float callBackWaitTime;
 
-	public DiscScript disc;
-	bool hasDisc = true;
+    private DiscScript disc;
+    private bool hasDisc = true;
 
-	void Update()
-	{
-		if(!IsOwner) return;
-
-		if(Input.GetKeyDown(KeyCode.Mouse0))
-		{
-			if(hasDisc)
-			{
-				handDisc.active = false;
-				SpawnDiscServerRpc();
-				hasDisc = false;
-			}
-			else
-			{
-				if(disc.inPlayerRange)
-				{
-					/*Destroy(disc.gameObject);
-					handDisc.active = true;
-					hasDisc = true;*/
-				}
-				else
-				{
-					if(disc != null)
-					{
-						StartCoroutine(CallBackDisc());
-						DestroyDisc(disc);
-					}
-				}
-			}
-		}
-
-		if(Input.GetKeyDown(KeyCode.Mouse1))
-		{
-			if(!hasDisc)
-			{
-				if(disc.inPlayerRange)
-				{
-					DestroyDisc(disc);
-					handDisc.active = true;
-					hasDisc = true;
-				}
-			}
-		}
-	}
-
-	private IEnumerator CallBackDisc()
-	{
-		yield return new WaitForSeconds(callBackWaitTime);
-		handDisc.active = true;
-		hasDisc = true;
-	}
-
-	[ServerRpc]
-    private void SpawnDiscServerRpc()
+    void Update()
     {
-        // Instantiate the disc on the server and then spawn it
-        var discInstance = Instantiate(discPrefab, throwLocation.transform.position, transform.rotation);
-        var networkObject = discInstance.GetComponent<NetworkObject>();
-        networkObject.Spawn(); // This ensures it gets networked
+        if (!IsOwner) return;
 
-        NotifyClientsOfDiscClientRpc(networkObject.NetworkObjectId, throwLocation.transform.position, transform.rotation);
-    }
-
-    private void DestroyDisc(DiscScript discToDestroy)
-    {
-        // This should be networked if you want it to destroy for all clients
-        if (discToDestroy != null)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            discToDestroy.gameObject.GetComponent<NetworkObject>().Despawn(); // Despawn the network object
-            disc = null; // Clear reference
+            if (hasDisc)
+            {
+                handDisc.SetActive(false);
+                SpawnDiscServerRpc(); // Call the RPC to spawn the disc
+                hasDisc = false;
+            }
+            else
+            {
+                if (disc != null && disc.inPlayerRange)
+                {
+                    // Optional: Logic for picking up the disc if it's in range
+                }
+                else
+                {
+                    StartCoroutine(CallBackDisc());
+                    RequestDespawnServerRpc(); // Call to request despawn on server
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (!hasDisc && disc != null && disc.inPlayerRange)
+            {
+                RequestDespawnServerRpc(); // Call to request despawn on server
+                handDisc.SetActive(true);
+                hasDisc = true;
+            }
         }
     }
 
-	[ClientRpc]
+    [ServerRpc]
+    private void SpawnDiscServerRpc()
+    {
+        var discInstance = Instantiate(discPrefab, throwLocation.position, transform.rotation);
+        var networkObject = discInstance.GetComponent<NetworkObject>();
+        networkObject.Spawn(); // This ensures it gets networked
+
+        // Notify clients of the disc
+        NotifyClientsOfDiscClientRpc(networkObject.NetworkObjectId, throwLocation.position, transform.rotation);
+    }
+
+    [ClientRpc]
     private void NotifyClientsOfDiscClientRpc(ulong networkObjectId, Vector3 position, Quaternion rotation)
     {
-        // Find the disc on the client using its NetworkObjectId
         var networkObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[networkObjectId];
         if (networkObject != null)
         {
             disc = networkObject.GetComponent<DiscScript>();
             if (disc != null)
             {
-                // Set its position and rotation
                 disc.transform.position = position;
                 disc.transform.rotation = rotation;
-                // You might also want to set its forward direction if needed
                 disc.SetForwardDirection(camera.transform.forward);
             }
         }
+    }
+
+    [ServerRpc]
+    private void RequestDespawnServerRpc()
+    {
+        DestroyDisc(); // Call the despawn logic on the server
+    }
+
+    private void DestroyDisc()
+    {
+        if (disc != null)
+        {
+            disc.gameObject.GetComponent<NetworkObject>().Despawn(); // Despawn the network object
+            disc = null; // Clear reference
+        }
+    }
+
+    private IEnumerator CallBackDisc()
+    {
+        yield return new WaitForSeconds(callBackWaitTime);
+        handDisc.SetActive(true);
+        hasDisc = true;
     }
 }
