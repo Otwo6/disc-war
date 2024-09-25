5580d00530dc2cfbafeb87610830cca63bc87c23
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-09-25 07:25:04 -0700

    Started Orange Coloring

--- Assets/Scripts/PlayerColorScript.cs
+++ Assets/Scripts/PlayerColorScript.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class PlayerColorScript : MonoBehaviour
 {
+    [SerializeField] Renderer discRen;
+    [SerializeField] Material orangeDiscMat;
+    public bool isOrange = false;
+
     void Start()
     {
         print(GameObject.FindGameObjectsWithTag("Player").Length);
+        if(GameObject.FindGameObjectsWithTag("Player").Length > 2)
+        {
+            discRen.material = orangeDiscMat;
+            isOrange = true;
+        }
     }
 }
